e7da5603615a2c69cdf79307184f332faf690489
Nicola <nicoanta95@gmail.com>
2024-04-18 15:55:34 +0200

    First new branch commit

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+// The game is organized as follows: the computer choice is taken from a choice list ["rock", "paper", "scissors"];
+// The computer makes its choice by picking a random index of the choice list;
+// One game round has the goal to determine the result of the round;
+
 function randomInteger(){
     return Math.floor(Math.random() * 3);
 };
 function getComputerChoice() {
-    let choice = ['rock', 'paper', 'scissors'];
+    let choice = ["rock", "paper", "scissors"];
     return choice[randomInteger()];
 };
 console.log(getComputerChoice());
+
 function gameRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection) {
         return "It's a tie!";
@@ -14,14 +19,18 @@ function gameRound(playerSelection, computerSelection){
         };
     return "You lose! " + computerSelection + " beats " + playerSelection;
 };
+
+// The results of each round have to be reported and updated at the end of each round, up until the match is over;
+// The match is composed of 5 rounds (ties are not counted); 
+
 function playGame(){
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 1; i <= 5; i++){
+    for (let i = 0; i < 5; i++){
         let computerChoice = getComputerChoice();
         let answer = prompt('Rock, paper or scissors? ').toLowerCase();
         if (!['rock', 'paper', 'scissors'].includes(answer)){
-            console.log('invalid answer, please make sure you have typed correctly!')
+            console.log('invalid choice, please make sure you have typed correctly!')
             i--;
             continue;
         };
