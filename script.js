2516425707d700a5ff8a4d4b23bfe8b2d196a83f
nicholsson <nicoanta95@gmail.com>
2024-04-19 10:48:11 +0200

    Add initial structure to index and style.css

--- script.js
+++ script.js
@@ -5,39 +5,31 @@ function getComputerChoice() {
     let choice = ['rock', 'paper', 'scissors'];
     return choice[randomInteger()];
 };
-console.log(getComputerChoice());
 function gameRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        return "Tie";
     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')){
-            return "You win! " + playerSelection + " beats " + computerSelection; 
+            return "Win"; 
         };
-    return "You lose! " + computerSelection + " beats " + playerSelection;
+    return "Lose";
 };
 function playGame(){
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 1; i <= 5; i++){
-        let computerChoice = getComputerChoice();
-        let answer = prompt('Rock, paper or scissors? ').toLowerCase();
-        if (!['rock', 'paper', 'scissors'].includes(answer)){
-            console.log('invalid answer, please make sure you have typed correctly!')
-            i--;
-            continue;
-        };
-        console.log('your choice: ' + answer);
-        console.log('computer\'s choice: ' + computerChoice);
-        let result = gameRound(answer, computerChoice);
-        if (result.includes('You win')) {
+    // Display choices here
+    let result = gameRound(answer, computerChoice);
+    if (result.includes('Win')) {
             playerScore++;
-        } else if (result.includes('You lose')) {
+    } else if (result.includes('Lose')) {
             computerScore++;
-        };
-        console.log('Score after round ' + i + ": Player = " + playerScore + ", Computer: " + computerScore);
+      };
+    // Display current score here
+
+
     };
     console.log('Final score:')
     console.log('Player: ' + playerScore);
     console.log('Computer: ' + computerScore);
 }
 //start of the game
-playGame();
+
