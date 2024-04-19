eb8460c19dbe5cc9f3fc0bac26051f8ffa5c2615
nicholsson <nicoanta95@gmail.com>
2024-04-19 21:08:13 +0200

    work on the script.js file to do

--- script.js
+++ script.js
@@ -16,20 +16,38 @@ function gameRound(playerSelection, computerSelection){
 function playGame(){
     let playerScore = 0;
     let computerScore = 0;
+    const playerChoiceContent = document.querySelector("#player-choice");
+    const computeChoiceContent = document.querySelector("#computer-choice");
+    const winner = document.querySelector("#match-results");
     // Display choices here
-    let result = gameRound(answer, computerChoice);
-    if (result.includes('Win')) {
-            playerScore++;
-    } else if (result.includes('Lose')) {
-            computerScore++;
-      };
-    // Display current score here
-
-
+    const choices = document.querySelectorAll(".btn");
+    choices.forEach(choice => {choice.addEventListener("click", () => {const playerChoice = choice.dataset.choice; playRound(playerChoice)})});
+    function playRound(playerChoice) {
+        let computerChoice = getComputerChoice();
+	playerChoiceContent.textContent = "You Chose:" +  playerChoice;
+	computerChoiceContent.textContent = "Computer choose:" + computerChoice;
+	let result = gameRound(playerChoice, computerChoice);
+        if (result.includes('Win')) {
+                playerScore++;
+        } else if (result.includes('Lose')) {
+                computerScore++;};
+	updateScores();
+	checkWinner();
     };
-    console.log('Final score:')
-    console.log('Player: ' + playerScore);
-    console.log('Computer: ' + computerScore);
-}
-//start of the game
+    // current score here
+    function updateScores() {
+	const computerScores = document.querySelector("#computer-score");
+	const playerScores = document.querySelector("#player-score");
+	playerScores.textContent = playerScore;
+	computerScores.textContent = computerScore;
+    };
+    function checkWinner() {
+	if(playerScore >= 5) {
+	    winner.textContent = "You won the match!";
+	} else if(computerScore >= 5) {
+	    winner.textContent = "Computer won the match";
+	};
+    };
+};
+playGame();
 
