6bb894e7b64211bcb32d17b21a520d8a9783d2cc
nicholsson <nicoanta95@gmail.com>
2024-04-20 10:23:19 +0200

    Game is working, but need to be able to reset the game after the match is finished

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function randomInteger(){
     return Math.floor(Math.random() * 3);
 };
 function getComputerChoice() {
-    let choice = ['rock', 'paper', 'scissors'];
+    let choice = ['Rock', 'Paper', 'Scissors'];
     return choice[randomInteger()];
 };
 function gameRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection) {
         return "Tie";
-    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')){
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Scissors' && computerSelection === 'Paper') || (playerSelection === 'Paper' && computerSelection === 'Rock')){
             return "Win"; 
         };
     return "Lose";
@@ -16,16 +16,17 @@ function gameRound(playerSelection, computerSelection){
 function playGame(){
     let playerScore = 0;
     let computerScore = 0;
-    const playerChoiceContent = document.querySelector("#player-choice");
-    const computeChoiceContent = document.querySelector("#computer-choice");
+    const playerChoiceContent = document.querySelector("#pchoice");
+    const computerChoiceContent = document.querySelector("#cchoice");
     const winner = document.querySelector("#match-results");
     // Display choices here
     const choices = document.querySelectorAll(".btn");
+// add event listener to each button
     choices.forEach(choice => {choice.addEventListener("click", () => {const playerChoice = choice.dataset.choice; playRound(playerChoice)})});
     function playRound(playerChoice) {
         let computerChoice = getComputerChoice();
-	playerChoiceContent.textContent = "You Chose:" +  playerChoice;
-	computerChoiceContent.textContent = "Computer choose:" + computerChoice;
+	playerChoiceContent.textContent = playerChoice;
+	computerChoiceContent.textContent = computerChoice;
 	let result = gameRound(playerChoice, computerChoice);
         if (result.includes('Win')) {
                 playerScore++;
@@ -34,10 +35,10 @@ function playGame(){
 	updateScores();
 	checkWinner();
     };
-    // current score here
+    //Update the current score here
     function updateScores() {
-	const computerScores = document.querySelector("#computer-score");
-	const playerScores = document.querySelector("#player-score");
+	const computerScores = document.querySelector("#cscore");
+	const playerScores = document.querySelector("#pscore");
 	playerScores.textContent = playerScore;
 	computerScores.textContent = computerScore;
     };
